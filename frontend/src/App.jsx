6d5683040ff33ce7ecca10af1ad65675c8b6cdbd
ugbeadie <ugbeadie3@gmail.com>
2026-07-13 23:08:45 +0100

    feat: redo frontend with js

---
+++ frontend/src/App.jsx
@@ -0,0 +1,7 @@
+import "./App.css";
+
+function App() {
+  return <p className="underline">new or</p>;
+}
+
+export default App;
--- frontend/src/App.tsx
+++
@@ -1,7 +0,0 @@
-import "./index.css";
-
-function App() {
-  return <p className="text-3xl font-bold underline">Hello, Vite + React!</p>;
-}
-
-export default App;
